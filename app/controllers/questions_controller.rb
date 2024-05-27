7d12cb5bdff7237631e68b40b08e04dc6c81a76c
Ricardo Tagliaverga tagliaverga@icloud.com <tagliaverga@icloud.com>
2024-05-27 15:56:56 -0300

    Coach

---
+++ app/controllers/questions_controller.rb
@@ -0,0 +1,18 @@
+class QuestionsController < ApplicationController
+  def ask
+
+  end
+
+  def answer
+    @answers = ['Great!', 'Silly question, get dressed and go to work!', "I don't care, get dressed and go to work!"];
+
+    @ask = params[:answer].strip
+    if @ask == "I am going to work";
+      @result = @answers[0]
+    elsif @ask[-1] == "?"
+      @result = @answers[1]
+    else
+      @result = @answers[2]
+    end
+  end
+end
